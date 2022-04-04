73e279e1651e397ecc163a45ea6ebfae9c38c0b5
Jakub Vítovec <vitovja1@fit.cvut.cz>
2022-04-04 12:12:40 +0200

    Server: ClientListener implemented into the ServerInterface

--- src/Server/ServerInterface.java
+++ src/Server/ServerInterface.java
@@ -1,11 +1,15 @@
 package Server;
 
+import Server.ClientServices.ClientListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashSet;
 
 //Server interface represents the running server
 public class ServerInterface {
     private Thread console;
-
     //Hash set representing id numbers of all users online
 
     private HashSet<Integer> online_ids;
@@ -19,5 +23,15 @@ public class ServerInterface {
     //Getter for the online users, mainly used by Console class
     public HashSet<Integer> getOnline_ids() {
         return online_ids;
+    }
+    //A function to listen for a client and add his id to the online set
+    public void Listen(int PortNumber){
+        ClientListener client = new ClientListener();
+        client.Listen(PortNumber);
+        Integer new_client = client.getClientID();
+        if (new_client != null){
+            this.online_ids.add(new_client);
+        }
+        //If the new_client is null than the listening was not sucesfull and we do nothing
     }
 }
