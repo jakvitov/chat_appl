91ed6f176ba300c9a8d5b2cdcbffa3b0a0fd10d5
Jakub Vítovec <vitovja1@fit.cvut.cz>
2022-04-04 12:07:22 +0200

    Server: ClientListener getClientID method implemented, to get ids from clients.

---
+++ src/Server/ClientServices/ClientListener.java
@@ -0,0 +1,54 @@
+package Server.ClientServices;
+
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ClientListener {
+    private Socket clientSocket;
+    private BufferedReader clientReader;
+    //A listener class that starts to listen on a given port, in the end it returns socket used to communicate
+    //with the client.
+    public void Listen(int PortNumber){
+        try {
+            ServerSocket serverSocket = new ServerSocket(PortNumber);
+            System.out.println("Server started listening on port: " + PortNumber);
+            this.clientSocket = serverSocket.accept();
+            System.out.println("Client connection established on port: " + PortNumber);
+            this.clientReader =
+                    new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));;
+        }
+        catch (
+        IOException IOE){
+            System.out.println("Error while opening the server socket.");
+            IOE.printStackTrace();
+        }
+    }
+    //A class used to get client id fromt the client
+    //The official version of the client message containg ID is \a\b<client_id>\a\b
+    public Integer getClientID (Socket clientSocket){
+        String suffix = "\\a\\b";
+        Integer result = 0;
+        try {
+            String input = this.clientReader.readLine();
+            if (input.length() <= 8 || !input.startsWith(suffix) || !input.endsWith(suffix)){
+                System.out.println("Wrong client input name format!");
+            }
+            input.replace(suffix, "");
+            result = Integer.parseInt(input);
+            return result;
+        }
+        catch (NumberFormatException NFE){
+            System.out.println("Client message includes characters!");
+            NFE.printStackTrace();
+        }
+        catch (IOException IOE){
+            System.out.println("Error while reading from the client socket!");
+            IOE.printStackTrace();
+        }
+        finally {
+            return null;
+        }
+    }
+}
