1b9d5f2ce348b9a00550f5f02352cf79a8c1d05a
Jakub Vítovec <vitovja1@fit.cvut.cz>
2022-04-12 21:08:33 +0200

    Client: Message listener implemented to the Interface

--- src/Client/ClientInterface.java
+++ src/Client/ClientInterface.java
@@ -11,12 +11,14 @@ import java.util.Scanner;
 
 public class ClientInterface {
     private Socket socket  = null;
+
+
     private PrintWriter clientWriter;
     private BufferedReader clientReader;
     private String clientID;
-
+    private Scanner scan;
     public ClientInterface (){
-        Scanner scan = new Scanner(System.in);
+        this.scan = new Scanner(System.in);
         System.out.println("Enter your client ID: ");
         this.clientID = scan.nextLine();
         try {
@@ -35,6 +37,9 @@ public class ClientInterface {
             System.out.println("Error while creating the client socket");
             System.exit(1);
         }
+    }
+    public BufferedReader getClientReader() {
+        return clientReader;
     }
     public void logIn() {
         System.out.println("Logging in as " + this.clientID);
@@ -55,8 +60,19 @@ public class ClientInterface {
         System.out.println("Logging out...");
         this.clientWriter.print("\\c\\l 101 LOGOUT \\c\\l");
     }
+    public void message(){
+        System.out.println("Insert ID of who you want to message: ");
+        String targetID = scan.nextLine();
+        System.out.println("Enter your message: ");
+        String message = scan.nextLine();
+        this.clientWriter.print(targetID + "\\c\\m" + message + "\\c\\m");
+    }
     public static void main(String[] args) {
         ClientInterface client = new ClientInterface();
+        //We launch the listener
+        Thread messageListener = new Thread(new MessageListener(client.getClientReader()));
         client.logIn();
+        client.message();
+        client.logOut();
     }
 }
